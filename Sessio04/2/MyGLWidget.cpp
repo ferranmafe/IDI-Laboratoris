fa14186e0fd2f4cd67d2596c88dd467a4620b2cc
Ferran Martinez Felipe <ferran.martinez.felipe@b5s101pc22.fib.upc.es>
2017-03-20 16:25:32 +0100

    Added some of the exercices from Sessio04

---
+++ Sessio04/2/MyGLWidget.cpp
@@ -0,0 +1,178 @@
+#include "MyGLWidget.h"
+
+#include <iostream>
+
+MyGLWidget::MyGLWidget (QWidget* parent) : QOpenGLWidget(parent)
+{
+  setFocusPolicy(Qt::ClickFocus);  // per rebre events de teclat
+  posx = 0.0f; posy = 0.0f;
+  angle = 0.0f;
+}
+
+MyGLWidget::~MyGLWidget ()
+{
+  if (program != NULL)
+    delete program;
+}
+
+void MyGLWidget::initializeGL ()
+{
+  // Cal inicialitzar l'ús de les funcions d'OpenGL
+  initializeOpenGLFunctions();  
+
+  glClearColor(0.5, 0.7, 1.0, 1.0); // defineix color de fons (d'esborrat)
+  carregaShaders();
+  createBuffers();
+  viewTransform();
+  projectTransform ();
+  modelTransform ();
+}
+
+void MyGLWidget::paintGL () 
+{
+  // Esborrem el frame-buffer
+  glClear (GL_COLOR_BUFFER_BIT);
+
+  // Carreguem la transformació de model
+  modelTransform ();
+
+  // Activem el VAO per a pintar la caseta 
+  glBindVertexArray (VAO_Casa);
+
+  // pintem
+  glDrawArrays(GL_TRIANGLE_STRIP, 0, 5);
+
+  glBindVertexArray (0);
+}
+
+void MyGLWidget::modelTransform () 
+{
+  // Matriu de transformació de model
+  glm::mat4 transform (1.0f);
+  transform = glm::rotate(transform, angle, glm::vec3(0.0f, 0.0f, 1.0f));
+  transform = transform * glm::translate(transform, glm::vec3(posx, posy, 0.0f));
+  glUniformMatrix4fv(transLoc, 1, GL_FALSE, &transform[0][0]);
+}
+
+void MyGLWidget::viewTransform ()
+{
+  // Matriu de projecciÃ³ del model. lookAt(obs, vrp, vup)
+  glm::mat4 view = glm::lookAt(glm::vec3(0.0, 0.0, 1.0),
+                               glm::vec3(0.0, 0.0, 0.0),
+                               glm::vec3(0.0, 1.0, 0.0));
+  glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
+}
+
+void MyGLWidget::projectTransform ()
+{
+  // Matriu de projecciÃ³ del model: perspective(fov (en rad), ra, znear, zfar)
+  glm::mat4 projecta = glm::perspective((float)M_PI/2.0f, 1.0f, 0.5f, 3.0f);
+  glUniformMatrix4fv(projectaLoc, 1, GL_FALSE, &projecta[0][0]);
+}
+
+void MyGLWidget::resizeGL (int w, int h) 
+{
+  glViewport(0, 0, w, h);
+}
+
+void MyGLWidget::keyPressEvent(QKeyEvent* event) 
+{
+  makeCurrent();
+  switch (event->key()) {
+    case Qt::Key_Left: {
+      posx -= 0.05;
+      angle += (float)M_PI/4.0f;
+      break;
+    }
+    case Qt::Key_Right: {
+      posx += 0.05;
+      angle += (float)M_PI/4.0f;
+      break;
+    }
+  case Qt::Key_Up: {
+    posy += 0.05;
+    angle += (float)M_PI/4.0f;
+    break;
+  }
+  case Qt::Key_Down: {
+    posy -= 0.05;
+    angle += (float)M_PI/4.0f;
+    break;
+  }
+    default: event->ignore(); break;
+  }
+  update();
+}
+
+void MyGLWidget::createBuffers () 
+{
+  // Dades de la caseta
+  // Dos VBOs, un amb posició i l'altre amb color
+  glm::vec3 posicio[5] = {
+	glm::vec3(-0.5, -1.0, -0.5),
+	glm::vec3( 0.5, -1.0, -0.5),
+	glm::vec3(-0.5,  0.0, -0.5),
+	glm::vec3( 0.5,  0.0, -0.5),
+	glm::vec3( 0.0,  0.6, -0.5)
+  }; 
+  glm::vec3 color[5] = {
+	glm::vec3(1,0,0),
+	glm::vec3(0,1,0),
+	glm::vec3(0,0,1),
+	glm::vec3(1,0,0),
+	glm::vec3(0,1,0)
+  };
+
+  // Creació del Vertex Array Object per pintar
+  glGenVertexArrays(1, &VAO_Casa);
+  glBindVertexArray(VAO_Casa);
+
+  glGenBuffers(1, &VBO_CasaPos);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO_CasaPos);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(posicio), posicio, GL_STATIC_DRAW);
+
+  // Activem l'atribut vertexLoc
+  glVertexAttribPointer(vertexLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
+  glEnableVertexAttribArray(vertexLoc);
+
+  glGenBuffers(1, &VBO_CasaCol);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO_CasaCol);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(color), color, GL_STATIC_DRAW);
+
+  // Activem l'atribut colorLoc
+  glVertexAttribPointer(colorLoc, 3, GL_FLOAT, GL_FALSE, 0, 0);
+  glEnableVertexAttribArray(colorLoc);
+
+  glBindVertexArray (0);
+}
+
+void MyGLWidget::carregaShaders()
+{
+  // Creem els shaders per al fragment shader i el vertex shader
+  QOpenGLShader fs (QOpenGLShader::Fragment, this);
+  QOpenGLShader vs (QOpenGLShader::Vertex, this);
+  // Carreguem el codi dels fitxers i els compilem
+  fs.compileSourceFile("shaders/fragshad.frag");
+  vs.compileSourceFile("shaders/vertshad.vert");
+  // Creem el program
+  program = new QOpenGLShaderProgram(this);
+  // Li afegim els shaders corresponents
+  program->addShader(&fs);
+  program->addShader(&vs);
+  // Linkem el program
+  program->link();
+  // Indiquem que aquest és el program que volem usar
+  program->bind();
+
+  // Obtenim identificador per a l'atribut “vertex” del vertex shader
+  vertexLoc = glGetAttribLocation (program->programId(), "vertex");
+  // Obtenim identificador per a l'atribut “color” del vertex shader
+  colorLoc = glGetAttribLocation (program->programId(), "color");
+  // Uniform locations
+  transLoc = glGetUniformLocation(program->programId(), "TG");
+  viewLoc = glGetUniformLocation(program->programId(), "view");
+  projectaLoc = glGetUniformLocation(program->programId(), "PR");
+}
+
+
+
